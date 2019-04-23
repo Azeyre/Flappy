fd01d562cd203768eae69736cb282ff6aa2fc294
Azeyre <aseyk2gaming@gmail.com>
2019-04-23 19:16:30 +0200

    Increase performance, and fix errors

--- src/graphics/Start.java
+++ src/graphics/Start.java
@@ -1,20 +1,18 @@
 package graphics;
 
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
 import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
 
 import javafx.application.Application;
-import javafx.geometry.Insets;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
@@ -26,8 +24,9 @@ public class Start extends Application {
 	private final int HEIGHT = 800, WIDTH = 800;
 	private GraphicsContext gc, pointGc;
 	private Bird bird;
-	private StackPane img, temp, pointPane;
-	private StackPane[] allPane;
+	private StackPane img, pointPane;
+	private ImageView temp1, temp2;
+	private ImageView[] pipesView;
 	private static boolean started = false;
 	private boolean finish = false;
 	private static Timer timer;
@@ -39,11 +38,8 @@ public class Start extends Application {
 		Group root = new Group(); //Groupe racine
 		img = new StackPane(); //StackPane comportant l'oiseau
 		pointPane = new StackPane();
-		allPane = new StackPane[3];
-		
-		allPane[0] = new StackPane();
-		allPane[1] = new StackPane();
-		allPane[2] = new StackPane();
+		pipesView = new ImageView[6];
+		createPipe();
 		
 		Scene scene = new Scene(root,WIDTH,HEIGHT);
 		
@@ -60,16 +56,31 @@ public class Start extends Application {
 		pointPane.getChildren().add(pointCanvas);
 		pointPane.setLayoutX(WIDTH / 2);
 		pointPane.setLayoutY(100);
-		root.getChildren().addAll(canvas, img , allPane[0], allPane[1], allPane[2], pointPane); //L'oiseau sera au dessus du canvas
+		root.getChildren().addAll(canvas, img , pipesView[0], pipesView[1],pipesView[2],pipesView[3],pipesView[4],pipesView[5], pointPane); //L'oiseau sera au dessus du canvas
 		
 		s.setTitle("Flappy Bird");
 		s.setResizable(false);
 		s.setScene(scene);
 		
-		createPipe();
 		addKeyHandler(scene);
 		
 		s.show();
+	}
+	
+	private void createPipe() throws FileNotFoundException {
+		for(int i = 0 ; i < 3 ; i++) {
+			Pipe p1 = new Pipe(0,0);
+			Pipe p2 = new Pipe(0,0);
+			
+			pipesView[(i*2)] = p1.up();
+			pipesView[(i*2) + 1] = p2.down();
+			
+			pipesView[(i*2)].setX(800 + (i * 400));
+			pipesView[(i*2)+1].setX(800 + (i * 400));
+			
+			pipesView[(i*2)].setY(rand(-700, -150));
+			pipesView[(i*2)+1].setY(pipesView[(i*2)].getY() + 700 + 150);
+		}
 	}
 	
 	public static void main(String[] args) {
@@ -78,72 +89,61 @@ public class Start extends Application {
 		System.exit(0);
 	}
 	
-	private void clear() {
-		gc.setFill(Color.RED);
-		gc.fillRect(0, 0, WIDTH, HEIGHT);
-	}
-	
 	private void addKeyHandler(Scene scene) {
 		scene.setOnKeyPressed(ke -> {
 			KeyCode keyCode = ke.getCode();
-			if(!started) {
-				started = true;
-				finish = false;
-				timer = new Timer();
-				timer.schedule(
-					    new TimerTask() {
-
-					        @Override
-					        public void run() {
-								bird.setY(bird.getY() + ySpeed);
-								img.setLayoutY(bird.getY());
-								if(img.getRotate() < 75) img.setRotate(img.getRotate() + 1.5);
-								ySpeed += 0.4;
-								for(int i = 0 ; i < allPane.length ; i++) {allPane[i].setLayoutX(allPane[i].getLayoutX() - 5);}
-								
-								if(bird.getY() < - 30 || bird.getY() > HEIGHT) {
-									started = false;
-									finish = true;
-									bird.setY(300);
-									img.setLayoutY(bird.getY());
-									img.setRotate(0);
-									timer.cancel();
-								} if(allPane[0].getLayoutX() < -80) {
-									allPane[0].setLayoutX(1200);
-									allPane[0].setLayoutY(rand(-700, -100));
-									temp = allPane[0];
-									allPane[0] = allPane[1];
-									allPane[1] = allPane[2];
-									allPane[2] = temp;
-								} if(bird.getX() == allPane[0].getLayoutX()) {
-									point++;
-									drawPoint();
-									System.out.println(point);
-								}
-								checkCollision();
-					        }
-					    }, 0, 16);
-			}
 			if(keyCode.equals(KeyCode.SPACE)) {
-				ySpeed = -7;
+				ySpeed = -9;
 				img.setRotate(-40);
+				loop();
 			}
 		});
 	}
 	
-	private void createPipe() throws FileNotFoundException {
-		Pipe temp1;
-		Pipe temp2;
-		VBox vbox;
-		for(int i = 0 ; i < allPane.length ; i++) {
-			temp1 = new Pipe(0,0);
-			temp2 = new Pipe(0,0);
-			vbox = new VBox();
-			vbox.setSpacing(150);
-			vbox.getChildren().addAll(temp1.up(),temp2.down());
-			allPane[i].getChildren().add(vbox);
-			allPane[i].setLayoutX(800 + (i * 400));
-			allPane[i].setLayoutY(rand(-700, 0));
+	private void loop() {
+		if(!started) {
+			started = true;
+			finish = false;
+			timer = new Timer();
+			timer.schedule(
+				    new TimerTask() {
+
+				        @Override
+				        public void run() {
+							bird.setY(bird.getY() + ySpeed);
+							img.setLayoutY(bird.getY());
+							if(img.getRotate() < 75) img.setRotate(img.getRotate() + 1.5);
+							ySpeed += 0.4;
+							for(int i = 0 ; i < pipesView.length ; i++) {pipesView[i].setX(pipesView[i].getX() - 5);}
+							
+							if(bird.getY() < - 30 || bird.getY() > HEIGHT) {
+								started = false;
+								finish = true;
+								bird.setY(300);
+								img.setLayoutY(bird.getY());
+								img.setRotate(0);
+								timer.cancel();
+							} if(pipesView[0].getX() < -80) {
+								pipesView[0].setX(1200);
+								pipesView[1].setX(1200);
+								pipesView[0].setY(rand(-700, -150));
+								pipesView[1].setY(pipesView[0].getY() + 700 + 150);
+								temp1 = pipesView[0];
+								temp2 = pipesView[1];
+								pipesView[0] = pipesView[2];
+								pipesView[1] = pipesView[3];
+								pipesView[2] = pipesView[4];
+								pipesView[3] = pipesView[5];
+								pipesView[4] = temp1;
+								pipesView[5] = temp2;
+							} if(bird.getX() == pipesView[0].getX()) {
+								point++;
+								drawPoint();
+								//System.out.println(point);
+							}
+							checkCollision();
+				        }
+				    }, 0, 16);
 		}
 	}
 	
@@ -158,12 +158,13 @@ public class Start extends Application {
 		pointGc.setFont(new Font("Arial bold", 68));
 		pointGc.fillText(Integer.toString(point), 0, 70);
 		pointGc.setStroke(Color.RED);
+		pointGc.setLineWidth(3);
 		pointGc.strokeText(Integer.toString(point), 0, 70);
 	}
 	
 	private boolean checkCollision() {
-		if(bird.getX() >= allPane[0].getLayoutX() - 50 && bird.getX() <= allPane[0].getLayoutX() + 100) {
-			if(bird.getY() <= allPane[0].getLayoutY() + 680 || bird.getY() >= allPane[0].getLayoutY() + 800) {
+		if(bird.getX() >= pipesView[0].getX() - 50 && bird.getX() <= pipesView[0].getX() + 100) {
+			if(bird.getY() <= pipesView[0].getY() + 680 || bird.getY() >= pipesView[1].getY() - 50) {
 				//System.out.println("HIT");
 				started = false;
 				finish = true;
@@ -173,9 +174,13 @@ public class Start extends Application {
 				timer.cancel();
 				point = 0;
 				drawPoint();
-				allPane[0].setLayoutX(800);
-				allPane[1].setLayoutX(1200);
-				allPane[2].setLayoutX(1600);
+				for(int i = 0 ; i < 3 ; i++) {					
+					pipesView[(i*2)].setX(800 + (i * 400));
+					pipesView[(i*2)+1].setX(800 + (i * 400));
+					
+					pipesView[(i*2)].setY(rand(-700, -150));
+					pipesView[(i*2)+1].setY(pipesView[(i*2)].getY() + 700 + 150);
+				}
 			}
 		}
 		return false;
